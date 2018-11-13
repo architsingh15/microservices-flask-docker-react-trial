fccb927b76b4184515b5135134bb243ef57681e1
Archit Singh <archit.singh@hashedin.com>
2018-11-13 13:14:46 +0530

    added flask blueprints and test setup for the service named users

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,12 +1,15 @@
 # services/users/manage.py
+
+
 import unittest
 
 from flask.cli import FlaskGroup
 
-from project import app
-from project import db
+from project import create_app, db
+from project.api.models import User
 
-cli = FlaskGroup(app)
+app = create_app()
+cli = FlaskGroup(create_app=create_app)
 
 
 @cli.command()
